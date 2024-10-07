72ee12fda04587bf42f29c5ebc89a1171ddc16db
Egor Gridnev <egnv.main@gmail.com>
2024-10-07 18:24:22 +0300

    create project

---
+++ src/main/java/org/my/finwise/FinwiseApplication.java
@@ -0,0 +1,13 @@
+package org.my.finwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinwiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinwiseApplication.class, args);
+    }
+
+}
